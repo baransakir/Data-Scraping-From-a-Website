ab92ab562ff0a8f34be1836294ffe06b40cbf51f
Baran Sakir <baransakirsw@gmail.com>
2022-08-09 05:50:18 +0300

    Update #1

--- scrape_data/scrape.py
+++ scrape_data/scrape.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import openpyxl
 
 def scrape_data(url):    
     webpage = requests.get(url)
@@ -11,25 +12,29 @@ def scrape_data(url):
     for row in rows:
         word = row.find(class_="column-1").text
         data.append(word)
-    return data[1:] # remove title row
+    return data[1:] # Remove title row
 
-def export_data(data):
+def export_data(data, col):
     df = pd.DataFrame(data)
-    df.to_excel("esp_dictionary.xlsx", header=False, index=False, encoding="utf-8")
+    try:
+        with pd.ExcelWriter("esp_dictionary.xlsx",engine="openpyxl",mode="a",if_sheet_exists="overlay") as writer:
+            df.to_excel(writer, sheet_name="Sheet1", header=False, index=False, encoding="utf-8", startcol=col)
+    except FileNotFoundError:   # Create excel file if not exists
+        df.to_excel("esp_dictionary.xlsx", sheet_name="Sheet1", header=False, index=False, encoding="utf-8", startcol=col)
 
 def next_letter(letter):
     return chr(ord(letter) + 1)
 
 def main():
+    col = 0
     letter = 'a'
-    words = []
     # Scrape data
     while(letter != next_letter('z')):
         letter_words = scrape_data("https://howismyspanish.com/spanish-words-start-with-" + letter + "/")
-        words += letter_words
         letter = next_letter(letter)
-    # Export data (to excel)
-    export_data(words)
+        # Export data (to excel)
+        export_data(letter_words, col)
+        col += 1
 
 if __name__ == "__main__":
     main()
